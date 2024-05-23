b1f3a086add62bfc804115d9cb43d8d8dc190ec4
TeuRocha <166167227+TeuRocha@users.noreply.github.com>
2024-05-23 13:06:48 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,108 +1,158 @@
 import * as React from 'react';
-import { TextInput, Text, View, Button } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {MaterialCommunityIcons} from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Card, Paragraph, Title } from 'react-native-paper';
+import { Button, Card, Paragraph, TextInput, Title } from 'react-native-paper';
+import { Audio } from 'expo-av';
 
 const Tab = createBottomTabNavigator();
-
 const Stack = createStackNavigator();
 
-class Principal extends React.Component{
-  constructor(props){
-    super(props)
+class Principal extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      usuario: undefined,
-      senha: undefined
-    }
+      usuario: '',
+      senha: '',
+    };
   }
 
-  render(){
-    return(
-    <View>
-      <Text>{"Usuário:"}</Text>
-      <TextInput onChangeText={(texto)=>this.setState({usuario: texto})}></TextInput>
-      <Text>{"Senha:"}</Text>
-      <TextInput onChangeText={(texto)=>this.setState({senha: texto})}></TextInput>
-      <Button title="Logar" onPress={()=>this.ler()}></Button>
-    </View>
-    )
-  }
-
-  async ler(){
-    try{
+  async ler() {
+    try {
       let senha = await AsyncStorage.getItem(this.state.usuario);
-      if(senha != null){
-        if(senha == this.state.senha){
-          //alert("Logado!!!");
-          this.props.navigation.navigate('Tela3')
-        }else{
+      if (senha != null) {
+        if (senha === this.state.senha) {
+          this.props.navigation.navigate('Teste1');
+        } else {
           alert("Senha Incorreta!");
         }
-      }else{
+      } else {
         alert("Usuário não foi encontrado!");
       }
-    }catch(erro){
+    } catch (erro) {
       console.log(erro);
     }
   }
+
+  render() {
+    return (
+      <View style={estilos.container}>
+        <Title>Login</Title>
+        <TextInput
+          label="Usuário"
+          mode="outlined"
+          style={estilos.input}
+          value={this.state.usuario}
+          onChangeText={(texto) => this.setState({ usuario: texto })}
+        />
+        <TextInput
+          label="Senha"
+          mode="outlined"
+          style={estilos.input}
+          secureTextEntry
+          value={this.state.senha}
+          onChangeText={(texto) => this.setState({ senha: texto })}
+        />
+        <Button mode="contained" onPress={() => this.ler()}>
+          Logar
+        </Button>
+      </View>
+    );
+  }
 }
 
-class Cadastro extends React.Component{
-  constructor(props){
+class Criar extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      user: undefined,
-      password: undefined,
-
-    }
-
+    this.state = {
+      user: '',
+      password: '',
+    };
   }
 
-  async gravar(){
-    try{
+  async playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+       require('/assets/som.mp3')
+    );
+    this.sound = sound;
+    await this.sound.playAsync();
+  }
+
+  async gravar() {
+    try {
       await AsyncStorage.setItem(this.state.user, this.state.password);
-      alert("Salvo com sucesso!!!")
-    }catch(erro){
-      alert("Erro!")
+      this.playSound();
+      alert("Salvo com sucesso!!!");
+    } catch (erro) {
+      alert("Erro!");
     }
   }
 
-  render(){
-    return(
-    <View>
-      <Text>{"Cadastrar Usuário:"}</Text>
-      <TextInput onChangeText={(texto)=>this.setState({user: texto})}></TextInput>
-      <Text>{"Cadastrar Senha:"}</Text>
-      <TextInput onChangeText={(texto)=>this.setState({password: texto})}></TextInput>
-      <Button title="Cadastrar" onPress={()=>this.gravar()}/>
-    </View>
-    )
+  componentWillUnmount() {
+    if (this.sound) {
+      this.sound.unloadAsync();
+    }
+  }
+
+  render() {
+    return (
+      <View style={estilos.container}>
+        <Title>Criar Usuário</Title>
+        <TextInput
+          label="Usuário"
+          mode="outlined"
+          style={estilos.input}
+          value={this.state.user}
+          onChangeText={(texto) => this.setState({ user: texto })}
+        />
+        <TextInput
+          label="Senha"
+          mode="outlined"
+          style={estilos.input}
+          secureTextEntry
+          value={this.state.password}
+          onChangeText={(texto) => this.setState({ password: texto })}
+        />
+        <Button mode="contained" onPress={() => this.gravar()}>
+          Criar
+        </Button>
+      </View>
+    );
   }
 }
 
 class Nav2 extends React.Component {
   render() {
     return (
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Principal} />
-          <Stack.Screen name="Tela3" component={Tela3}/>
-       </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Principal} />
+        <Stack.Screen name="Teste1" component={Teste1} options={{ headerShown: false }} />
+        <Stack.Screen name="Teste2" component={Teste2} options={{ headerShown: false }} />
+        <Stack.Screen name="Teste3" component={Teste3} options={{ headerShown: false }} />
+      </Stack.Navigator>
     );
   }
 }
 
-class Tela3 extends React.Component {
-    render() {
+class Teste1 extends React.Component {
+  render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-         <Card>
-          <Card.Title title="Bem vindo"  />
-          <Card.Content>
-            <Paragraph>{'Tela 3'}</Paragraph>
+      <View style={estilos.container}>
+        <Card>
+          <Card.Content>            
+            <Image 
+              source={require('/assets/image1.png')} 
+              style={estilos.imagem} 
+            />
+            <Button mode="contained" onPress={() => this.props.navigation.navigate('Teste2')}>
+              Próximo
+            </Button>
+            <Button mode="outlined" onPress={() => this.props.navigation.goBack()}>
+              Voltar
+            </Button>
           </Card.Content>
         </Card>
       </View>
@@ -110,43 +160,119 @@ class Tela3 extends React.Component {
   }
 }
 
-class Informacoes extends React.Component{
-  render(){
-    return(
-    <View>
-      <Text>{"Mateus advertências!"}</Text>
-      <Text>{" "}</Text>
-      <Text>{
-        " Este teste não é 100% seguro e pode dar um falso positivo. Ele pode variar com a regulagem do seu monitor que pode mostrar cores diferentes do teste original. Existem várias versões desse teste, eu peguei apenas 6 desenhos. Esse teste só funciona para pessoas que não enxergam direito vermelho ou verde. Caso você perceber que não enxerga direito alguma cor, recomendo fazer um teste profissional em um Oftalmologista."
-      }</Text>
-      <Button title="Voltar" onPress={()=>this.props.navigation.goBack()}></Button>
-    </View>
-    )
+class Teste2 extends React.Component {
+  render() {
+    return (
+      <View style={estilos.container}>
+        <Card>
+          <Card.Content>
+            <Image 
+              source={require('/assets/image2.png')} 
+              style={estilos.imagem} 
+            />
+            <Button mode="contained" onPress={() => this.props.navigation.navigate('Teste3')}>
+              Próximo
+            </Button>
+            <Button mode="outlined" onPress={() => this.props.navigation.goBack()}>
+              Voltar
+            </Button>
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  }
+}
+
+class Teste3 extends React.Component {
+  render() {
+    return (
+      <View style={estilos.container}>
+        <Card>
+          <Card.Content>
+            <Image 
+              source={require('/assets/image3.png')} 
+              style={estilos.imagem} 
+            />         
+            <Button mode="contained" onPress={() => this.props.navigation.navigate('Informações')}>
+              Próximo
+            </Button>
+            <Button mode="outlined" onPress={() => this.props.navigation.goBack()}>
+              Voltar
+            </Button>
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  }
+}
+
+class Informacoes extends React.Component {
+  render() {
+    return (
+      <View style={estilos.container}>
+        <Title>Informações</Title>
+        <Paragraph>
+          {"Mateus advertências!"}
+        </Paragraph>
+        <Paragraph>
+          {
+            "Este teste não é 100% seguro e pode dar um falso positivo. Ele pode variar com a regulagem do seu monitor que pode mostrar cores diferentes do teste original. Existem várias versões desse teste, eu peguei apenas 3 desenhos. Esse teste só funciona para pessoas que não enxergam direito vermelho ou verde. Caso você perceber que não enxerga direito alguma cor, recomendo fazer um teste profissional em um Oftalmologista."
+          }
+        </Paragraph>
+      </View>
+    );
   }
 }
 
 class App extends React.Component {
-
   render() {
-    return(
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Login" component={Nav2} 
-          options={{
-            tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="home-account" color={color} size={size}/>)
-            , headerShown: false
-          }}
-        />
-        <Tab.Screen name="Criar Usuário" component={Cadastro}
-          options={{
-            tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="account-details" color={color} size={size}/>)
-          }}
-        />
-        <Tab.Screen name="Informacoes" component={Informacoes} />
-      </Tab.Navigator>
-    </NavigationContainer>
-    )
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Login"
+            component={Nav2}
+            options={{
+              tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home-account" color={color} size={size} />),
+              headerShown: false,
+            }}
+          />
+          <Tab.Screen
+            name="Criar Usuário"
+            component={Criar}
+            options={{
+              tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="account-details" color={color} size={size} />)
+            }}
+          />
+          <Tab.Screen
+            name="Informações"
+            component={Informacoes} 
+            options={{
+              tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="information" color={color} size={size} />)
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
   }
 }
 
 export default App;
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    marginBottom: 16,
+    marginHorizontal: 16,
+  },
+  imagem: {
+    width: 300,
+    height: 300,
+    alignSelf: 'center',
+    marginVertical: 16,
+  },
+});
